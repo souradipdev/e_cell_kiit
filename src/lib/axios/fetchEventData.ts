87d5cccc469e1a2f67e864c71793e4450e7a2058
souradipdev <22052939@kiit.ac.in>
2024-11-04 16:56:34 +0530

    Integrated event api route with frontend

---
+++ src/lib/axios/fetchEventData.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+async function wait() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("")
+    }, 500);
+  })
+}
+
+export async function fetchEventData() {
+  await wait();
+  try {
+    const response = await axios.get(`${process.env.HOST_NAME}/api/events`);
+
+    return response.data.events;
+  } catch (error) {
+    console.error("Error fetching hero section data:", error);
+
+    return {
+      error: true,
+      message: "Unable to load the Event section. Please try again later.",
+    };
+  }
+}
