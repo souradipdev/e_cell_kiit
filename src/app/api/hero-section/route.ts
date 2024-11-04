a08d21017b4411d3aaf6bd92bd7ba0a0d417437a
souradipdev <22052939@kiit.ac.in>
2024-11-04 14:18:58 +0530

    Added heroSection api route

---
+++ src/app/api/hero-section/route.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import fs from "fs";
+
+export async function GET() {
+  const filePath = path.join(process.cwd(), "src/data", "heroSection.json");
+  try{
+    const dataResponse = await fs.promises.readFile(filePath, "utf-8");
+    return Response.json(JSON.parse(dataResponse), {status: 200});
+  } catch (error) {
+    console.log(error);
+
+    return Response.json({error}, {status: 500});
+  }
+}
